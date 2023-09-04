88eda60036fb7b5cbb649ae059779070d4a5b190
djami <djamilatraore31@gmail.com>
2023-09-04 16:33:34 +0000

    About fini

--- About.php
+++ About.php
@@ -88,4 +88,74 @@
         <!-- <embed src="SaveTube.App-Des Paroles et D-CLIC - Rencontre avec Ilane, 23 ans originaire de Madagascar-(1080p).mp4" class="c"> -->
         </div>
         </section>
+                
+        <section class="blog">
+      <h1>BLOG</h1>
+    <div class="blocs">
+    <div class="blocus">
+    <img src="images/learn.jpg" alt="">
+    <h6><strong>Accessible</strong> <br>24h/24</h6>
+    <h5>Recrutements/Entreprises</h5>
+    <p>Missions</p>
+    <small><i class="bi bi-people-fill"></i>Connexions</small>
+    <small><i class="bi bi-wechat"></i>Commentaires</small>
+    </div>
+    <div class="bloc">
+      <h3>ARTICLES</h3>
+    <div class="card">
+    <img src="images/pb.png" alt="">
+    <a href="#">les 8 principaux conseils sur le personnal Branding</a>
+    </div>
+      <div class="card">
+        <img src="images/dclic-b.jpg" alt="">
+        <div class="text">
+        <a href="#"> Entreprendre dans le Domaine du numerique</a>
+        <p>C'est possible grace a D-Clic </p>
+        </div>
+        </div>
+    </div>
+    </div>
+    </section>
+    <?php include("footer.php") ?>
+
+<!-- <section class="omni">
+    <section class="footer">
+      <div class="img-footer">
+    <img src="download.png" alt="">
+    <p>SUIVEZ-NOUS-SUR</p>
+    <a href="#"><i class="ri-facebook-fill"></i></a>
+    <a href="#"><i class="ri-instagram-fill"></i></a>
+    <a href="#"><i class="ri-youtube-fill"></i></a>
+    <i class="ri-github-fill"></i>
+    </div>
+    <div class="RA">
+       <p>Organisation internationale de la Francophonie</p>
+    <p>19-21 avenue Bosquet.75007 Paris(France)</p>
+    <p>Telephone (33)1 44 37 33 25</p>
+    <p>Telecopie (33) 1 45 79 14 98</p>
+    </div>
+    <div class="rachi">
+      <p class="P">RECRUTEMENTS</p>
+      <P>APPELS A</P>
+      <P>CANDIDATURES</P>
+      <P>MARCHES PUBLICS</P>
+    </div> 
+    <div class="icons">
+      <p>CONTACTS</p>
+      <P>MENTIONS LEGALES</P>
+      <P>POLITIQUE DE CONFIDENTIALITE</P>
+    </div>
+</div>
+    </section>
+    </section> 
+    <section class="L">
+    <div class="l">Copyright © francophone 2023. All Rights Reserved</div>
+    </section> -->
+    
+
+    <script src="index.js"></script>
+
+        
+    </body>
+    </html>
         
---
+++ Services.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9"
+    crossorigin="anonymous"
+  />
+  <script
+    defer
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm"
+    crossorigin="anonymous"
+  ></script>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2? family*Rubik:wght@400;500;600;700;etdisplay*swap" rel="stylesheet">
+    <!-- box icons link -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <!-- remix icons link -->
+    <link href="http://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <title>Document</title>
+    <link rel="stylesheet" href="Services.css">
+</head>
+<body>
+
+    <section class="hero-section">
+        <header>
+        <?php include("menu.php") ?>
+
+           <!-- <nav>
+                <div class="logo">
+                    <img src="download.png" alt="">
+                </div>
+                 <ul class="menu">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="About.html">About</a></li>
+                    <li><a href="Services.html">Services</a></li>
+                    <li><a href="Contact.html">Contact</a></li>
+                </ul> 
+            </nav>-->
+        </header>
+    </section>
+
+
+    <section class="innerpage bg-yellow">
+    <div class="container">
+    <div class="row">
+    <div class="col-12 col-lg-11 ml-auto">
+    <div class="inner-content text-white">
+    <small>PROGRAMME</small>
+    <h2 class="Libre-bold text-white mb-0">D-CLIC, formez-vous au numérique</h2>
+    </div>
+</div>
+</div>
+</div>
+</section>
+<section class="container1">
+<div class="img">
+<img src="Educ filles Tchad 1.jpg" alt="">
+</div>
+<h1>Depuis plus de vingt ans, l’OIF déploie des efforts en faveur de la réduction des inégalités de genre dans ses différents domaines d’intervention au sein de l’espace francophone.</h1>
+<p>Il y a aujourd’hui près de 140 millions de femmes vivant dans les pays francophones ; elles seront plus de 350 millions en 2050. Malgré de nombreuses avancées au niveau législatif ou dans le domaine de l’éducation, un ralentissement des progrès en faveur des droits des femmes et des filles, et parfois même leur remise en cause, est à noter. L’OIF œuvre ainsi pour préserver les acquis et éliminer les défis persistants de la mise en œuvre des engagements nationaux et internationaux en faveur de l’égalité femmes-hommes, des droits et de l’autonomisation des femmes et des filles.</p>
+</section>
+<!-- <div class="F"> -->
+<section class="A">
+<div id="carouselExampleCaptions" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="Sans titre 322.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+        <h1>La Francophonie avec Elles</h1>
+       <p>Le Fonds La Francophonie avec Elles a pour objectif de donne aux femmes et aux filles <br> les moyens de renforcer leur acces au developpement economique,a l'education,a la sante</p>
+    </div>
+      </div>
+      <div class="carousel-item">
+        <img src="Sans titre 322.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Des Paroles et D-CLIC-Rencontre <br> avecIlane, 23 ans, formee en <br> developpement Web par SAYNA.</h1>
+          <p>Ilane participe au programme D-CLIC.Elle est formee par l'operateur malgache SAYNA.a la <br> suit de sa formation elle souhaite devenir developpeuse Web.</p>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src="Sans titre 322.jpg" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h1>Des Paroles et D-CLIC-Rencontre <br> avecIlane, 23 ans, formee en <br> developpement Web par SAYNA.</h1>
+          <p>Ilane participe au programme D-CLIC.Elle est formee par l'operateur malgache SAYNA.a la <br> suit de sa formation elle souhaite devenir developpeuse Web.</p>
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  </section>
+<!-- </div> -->
+  
